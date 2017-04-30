bde4a6c5f63ea52bad70f54e1344e2bbefc43822
Roni Yusuf Manalu <rymanalu@gmail.com>
2017-04-30 22:59:11 +0700

    Create UploaderManager test script.

---
+++ tests/UploaderManagerTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use Mockery as m;
+use Illuminate\Contracts\Container\Container;
+use Rymanalu\LaravelSimpleUploader\UploaderManager;
+use Rymanalu\LaravelSimpleUploader\Contracts\Provider;
+use Rymanalu\LaravelSimpleUploader\Providers\LocalProvider;
+use Illuminate\Contracts\Config\Repository as ConfigContract;
+use Illuminate\Contracts\Filesystem\Filesystem as FilesystemContract;
+use Illuminate\Contracts\Filesystem\Factory as FilesystemManagerContract;
+
+class UploaderManagerTest extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testCreateUploaderInstanceByResolvingTheRequiredInstanceFromContainer()
+    {
+        $config = new Config([
+            'local' => m::mock(Provider::class),
+            'request' => m::mock(Provider::class),
+        ]);
+
+        $app = m::mock(Container::class);
+        $app->shouldReceive('make')->with('config')->andReturn($config);
+        $app->shouldReceive('make')->with('filesystem')->andReturn(new FilesystemManager);
+        $app->shouldReceive('make')->with(LocalProvider::class)->andReturn($config->get('local'));
+
+        $uploaderManager = new UploaderManager($app);
+        $uploaderManager->from('local');
+    }
+}
+
+class Config implements ConfigContract
+{
+    protected $items = [];
+
+    public function __construct(array $items = [])
+    {
+        $this->items = $items;
+    }
+
+    public function has($key)
+    {
+        return isset($this->items[$key]);
+    }
+
+    public function get($key, $default = null)
+    {
+        if ($this->has($key)) {
+            return $this->items[$key];
+        }
+
+        return $default;
+    }
+
+    public function all()
+    {
+        return $this->items;
+    }
+
+    public function set($key, $value = null)
+    {
+        $this->items[$key] = $value;
+    }
+
+    public function prepend($key, $value)
+    {
+        //
+    }
+
+    public function push($key, $value)
+    {
+        //
+    }
+}
+
+class FilesystemManager implements FilesystemManagerContract
+{
+    public function disk($name = null)
+    {
+        return m::mock(FilesystemContract::class);
+    }
+}
